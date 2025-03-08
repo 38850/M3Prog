2a5daccc813039c50f7824d76ac383355da83848
38850 <38850@ma-web.nl>
2025-03-08 14:16:11 +0100

    03_commit 3

--- m3prog_project/public/03/reiskosten.php
+++ m3prog_project/public/03/reiskosten.php
@@ -1,11 +1,9 @@
 <?php
-
-$afstand = 928.45; 
-$prijs_per_liter = 1.89; 
-$prijs_trein = 60.00; 
-$tankinhoud = 50; 
-$verbruik_per_km = 1 / 15; 
-
+$afstand = 928.45;
+$prijs_per_liter = 1.89;
+$prijs_trein = 60.00;
+$tankinhoud = 50;
+$verbruik_per_km = 1 / 15;
 
 $liter_verbruikt = $afstand * $verbruik_per_km;
 $aantal_tankbeurten = ceil($liter_verbruikt / $tankinhoud);
@@ -29,10 +27,10 @@ if ($kosten_auto > $prijs_trein)
 </head>
 <body>
     <h2>Reiskosten Berekening</h2>
-    <p>Totale afstand: <?= number_format($afstand, 2); ?> km</p>
-    <p>Totale kosten met de auto: €<?= number_format($kosten_auto, 2); ?></p>
-    <p>Aantal liter benzine verbruikt: <?= number_format($liter_verbruikt, 2); ?> liter</p>
-    <p>Aantal keren tanken: <?= $aantal_tankbeurten; ?></p>
-    <p>Keuze: <?= $keuze; ?></p>
+    <p><strong>Totale afstand:</strong> <?= $afstand; ?> km</p>
+    <p><strong>Totale kosten met de auto:</strong> €<?= $kosten_auto; ?></p>
+    <p><strong>Aantal liter benzine verbruikt:</strong> <?= $liter_verbruikt; ?> liter</p>
+    <p><strong>Aantal keren tanken:</strong> <?= $aantal_tankbeurten; ?></p>
+    <p><strong>Keuze:</strong> <?= $keuze; ?></p>
 </body>
 </html>
